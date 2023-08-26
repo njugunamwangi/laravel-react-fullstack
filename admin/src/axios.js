317d99c75310cf8116ce439bafe7f045facb847a
njugunamwangi <njugunamwangi.n@gmail.com>
2023-08-27 02:03:02 +0300

    Update axios

--- admin/src/axios.js
+++ admin/src/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import router from "./router.jsx";
 
 const axiosClient = axios.create({
     baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
@@ -18,6 +19,8 @@ axiosClient.interceptors.response.use((response) => {
 
     if (response.status === 401) {
         localStorage.removeItem('ACCESS_TOKEN')
+        window.location.reload()
+        router.navigate('/login')
     }
 
     throw error
