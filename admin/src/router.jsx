966c764ef0aeac7167cc427abf839a87801aa9f7
njugunamwangi <njugunamwangi.n@gmail.com>
2023-08-26 14:14:31 +0300

    Base files

--- admin/src/router.jsx
+++ admin/src/router.jsx
@@ -1,21 +1,44 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, Navigate} from "react-router-dom";
 import Login from "./views/Login.jsx";
 import Signup from "./views/Signup.jsx";
 import Users from "./views/Users.jsx";
-import NotFound from "./views/NotFound.jsx";
+import NotFound from "./views/components/NotFound.jsx";
+import AuthLayout from "./views/components/AuthLayout.jsx";
+import AdminLayout from "./views/components/AdminLayout.jsx";
+import Dashboard from "./views/Dashboard.jsx";
 
 const router = createBrowserRouter([
     {
-        path: '/login',
-        element: <Login />
+        path: '/',
+        element: <AdminLayout />,
+        children: [
+            {
+                path: '/dashboard',
+                element: <Navigate to="/" />
+            },
+            {
+                path: '/',
+                element: <Dashboard />
+            },
+            {
+                path: '/users',
+                element: <Users />
+            }
+        ]
     },
     {
-        path: '/signup',
-        element: <Signup />
-    },
-    {
-        path: '/users',
-        element: <Users />
+        path: '/',
+        element: <AuthLayout />,
+        children: [
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/signup',
+                element: <Signup />
+            },
+        ]
     },
     {
         path: '*',
