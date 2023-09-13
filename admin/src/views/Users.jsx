9c8957127caa4866f25aed22ab11ceafbe61ee79
njugunamwangi <njugunamwangi.n@gmail.com>
2023-09-13 22:06:17 +0300

    List users

--- admin/src/views/Users.jsx
+++ admin/src/views/Users.jsx
@@ -1,10 +1,51 @@
 import AdminComponent from "./components/AdminComponent.jsx";
+import axiosClient from "../axios.js";
+import {useEffect, useState} from "react";
+import {PlusCircleIcon} from "@heroicons/react/20/solid/index.js";
+import TButton from "./components/core/TButton.jsx";
+import {useStateContext} from "./contexts/ContextProvider.jsx";
+import Loading from "./components/core/Loading.jsx";
+import UserList from "./lists/UserList.jsx";
 
 export default function Users() {
+    const { showToast } = useStateContext()
+
+    const [ users, setUsers ] = useState([])
+
+    const [ loading, setLoading ] = useState(false)
+    const getUsers = () => {
+        setLoading(true)
+        axiosClient.get('/users')
+            .then(({ data }) => {
+                setUsers(data.data)
+                setLoading(false)
+            })
+    }
+
+    useEffect(() => {
+        getUsers()
+    }, [])
+
     return (
         <>
-            <AdminComponent title="Users">
-                Users
+            <AdminComponent
+                title="Users"
+                buttons={
+                    <TButton color="green" to="/users/add">
+                        <PlusCircleIcon className="h-6 w-6 mr-2" />
+                        Add User
+                    </TButton>
+                }
+            >
+                {loading && ( <Loading /> )}
+
+                {!loading && (
+                    <ul role="list" className="divide-y divide-gray-100">
+                        {users.map((user) => (
+                            <UserList user={user} key={user.id} />
+                        ))}
+                    </ul>
+                )}
             </AdminComponent>
         </>
     )
